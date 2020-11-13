995cdac1fb5310c430ffdae4e0f58d7cc579cf0a
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-11-13 12:00:44 -0600

    Section 9 start... Get finished

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,19 +9,35 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data})
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts})
             });
+    }
+
+    postSelectedHandler=(id) => {
+        this.setState({selectedPostId: id});
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />
+            return <Post 
+                key={post.id} 
+                author={post.author}
+                title={post.title} 
+                clicked={() => this.postSelectedHandler(post.id)}/>
         });
 
         return (
@@ -30,7 +46,7 @@ class Blog extends Component {
                 {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
