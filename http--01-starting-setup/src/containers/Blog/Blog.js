73d5733f56d9015a3be4181b1bb1464585fe85c3
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-11-16 17:30:01 -0600

    Sec 9 complete axios intercepts and instances

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -10,11 +11,12 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
@@ -24,6 +26,9 @@ class Blog extends Component {
                     }
                 })
                 this.setState({posts: updatedPosts})
+            })
+            .catch(error => {
+                this.setState({error: true})
             });
     }
 
@@ -32,13 +37,17 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                author={post.author}
-                title={post.title} 
-                clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+        let posts = <p style={{color: 'red', textAlign: 'center'}}>Something went wrong</p>;
+
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    author={post.author}
+                    title={post.title} 
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
 
         return (
             <div>
