7ba39f510380d977319e93993a92614053b75bee
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-11-19 17:34:36 -0600

    Sec 10 Accessing server complete

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.6,
@@ -17,16 +18,22 @@ const INGREDIENT_PRICES = {
 class BurgerBuilder extends Component {
 
     state = {
-        ingredients:{
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients:null,
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+    }
+
+    componentDidMount(){
+
+        axios.get('/ingredients.json')
+            .then(res => {
+                this.setState({ingredients: res.data});
+            })
+            .catch(error => {
+                this.setState({error: true});
+            });
     }
 
     updatePurchaseState(ingredients){
@@ -147,9 +154,26 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
 
-        let orderSummary = <OrderSummary ingredients={this.state.ingredients} cancel={this.purchaseCancelHandler} price={this.state.totalPrice} continue={this.purchaseContinueHandler}/>;
+        let orderSummary = null;
+        let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner/>;
 
-        if(this.state.loading){
+        if(this.state.ingredients){
+
+            orderSummary = <OrderSummary ingredients={this.state.ingredients} cancel={this.purchaseCancelHandler} price={this.state.totalPrice} continue={this.purchaseContinueHandler}/>;
+
+            burger = <Auxil>
+                        <Burger ingredients={this.state.ingredients}/>
+                        <BuildControls ingredients={this.state.ingredients}
+                            add={this.addIngredientHandler}
+                            remove={this.removeIngredientHandler}
+                            totalPrice={this.state.totalPrice}
+                            disabled={disabledInfo}
+                            purchasable={this.state.purchasable}
+                            order={this.purchaseHandler}/>
+                    </Auxil>;
+        }
+
+        if(this.state.loadingOrders){
             orderSummary = <Spinner/>;
         }
 
@@ -158,18 +182,11 @@ class BurgerBuilder extends Component {
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <div>Burger Display</div>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls ingredients={this.state.ingredients}
-                                add={this.addIngredientHandler}
-                                remove={this.removeIngredientHandler}
-                                totalPrice={this.state.totalPrice}
-                                disabled={disabledInfo}
-                                purchasable={this.state.purchasable}
-                                order={this.purchaseHandler}/>
+                {burger}
+                
             </Auxil>
         );
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
--- burger-app/src/hoc/Layout/Layout.js
+++ burger-app/src/hoc/Layout/Layout.js
@@ -6,7 +6,6 @@ import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 const layout = (props) => (
     <Auxil>
         <Toolbar/>
-        <div>toolbar, sidedrawer, backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
