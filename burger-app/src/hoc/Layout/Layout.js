3d5f409bbf5c6c08e369acbc997ebe8587db924c
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-12-01 16:38:27 -0600

    implement drawer

--- burger-app/src/hoc/Layout/Layout.js
+++ burger-app/src/hoc/Layout/Layout.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxil from '../Auxil/Auxil';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxil>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxil>
-);
+class Layout extends Component{
 
-export default layout;
+    state = {
+        showDrawer: false
+    }
+
+    closeDrawer = () => {
+        this.setState({showDrawer: false});
+    }
+
+    openDrawer = () => {
+        this.setState({showDrawer: true});
+    }
+
+    render(){
+        return(
+            <Auxil>
+                <Toolbar openSideDrawer={ this.openDrawer }/>
+                { this.state.showDrawer ? <SideDrawer open close={this.closeDrawer}/> : null }
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxil>
+        )
+    }
+};
+
+export default Layout;
