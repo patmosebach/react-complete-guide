9b33576fe2183e639939043c185b2c35e1f76e1d
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2021-01-05 16:23:47 -0600

    Forms and Validation start

--- burger-app/src/containers/Contact/Contact.js
+++ burger-app/src/containers/Contact/Contact.js
@@ -3,11 +3,64 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Contact.css';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import Input from '../../components/UI/Input/Input';
 
 
 class Contact extends Component {
 
     state = {
+        orderForm: {            
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },                
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street Addy'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zipcode'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },                
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },            
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
+        },
         name: '',
         email: '',
         address: {
@@ -25,16 +78,7 @@ class Contact extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            customer: {
-                name: 'Pat',
-                address: {
-                    stree: 'Teststreet',
-                    zipCode: '60561',
-                    country: 'US'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+
         }
 
         axios.post('/orders.json', order)
@@ -48,14 +92,18 @@ class Contact extends Component {
     }
 
     render(){
+
+        let inputs = 
+            Object.values(this.state.orderForm).map((el, i) => { 
+                return <Input key={i} elementType={el.elementType} elementConfig={el.elementConfig} value={el.value} />
+            });
+        
+
         let form =(
             
                 
                 <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street Address" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                    {inputs}
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
             
